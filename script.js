a097fe81882fb7e78e628d5187ff66f76d634a9c
Ken Morton Jr <kmorton15@gmail.com>
2019-09-21 20:10:49 -0400

    added check for hitting github api rate limit

--- script.js
+++ script.js
@@ -2,25 +2,22 @@ let apiURL = 'https://api.github.com/repos/kjmj/avocados';
 Vue.component('v-select', VueSelect.VueSelect);
 
 var githubCommits = new Vue({
-
   el: '#githubCommits',
-
   data: {
     branches: [''],
     currentBranch: '',
-    commits: ''
+    commits: '',
+    userMessage: 'test'
   },
-
   created: function () {
+    this.checkValidRepo();
     this.getDefaultBranch();
     this.getBranches();
     this.getCommits();
   },
-
   watch: {
     currentBranch: 'getCommits'
   },
-
   filters: {
     truncate: function (v) {
       let newline = v.indexOf('\n');
@@ -30,8 +27,23 @@ var githubCommits = new Vue({
       return v.replace(/T|Z/g, ' ');
     }
   },
-
   methods: {
+    // check if the specified repo is a valid one
+    checkValidRepo: function() {
+      let self = this;
+
+      self.makeRequest(apiURL)
+      .then(function (response) {
+        // github api limit was hit
+        if(response.status === 403) {
+          self.userMessage = 'You have likely hit your github api call limit. Please see https://developer.github.com/v3/#rate-limiting for more info'
+        }
+      })
+      .catch(function (error) {
+        console.log('Error checking valid repo', error);
+      });
+    },
+    // get the default branch of the given github repo
     getDefaultBranch: function() {
       let self = this;
 
@@ -43,6 +55,7 @@ var githubCommits = new Vue({
             console.log('Error fetching default branch: ', error);
           });
     },
+    // get all the branches of the given github repo
     getBranches: function () {
       let self = this;
       let queryString = '/branches';
@@ -55,6 +68,7 @@ var githubCommits = new Vue({
             console.log('Error fetching branches: ', error);
           });
     },
+    // get the commits of the given github repo
     getCommits: function () {
       let self = this;
       let queryString = '/commits?per_page=4&sha=';
@@ -67,6 +81,7 @@ var githubCommits = new Vue({
             console.log('Error fetching commits: ', error);
           });
     },
+    // async helper function to make a XMLHttpRequest. returns a promise
     makeRequest: function (url) {
       let xhr = new XMLHttpRequest();
 
